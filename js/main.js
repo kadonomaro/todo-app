57fe05ef152cea4c1a3dfadcfde16fe849189a44
kadonomaro <denis.afer@gmail.com>
2019-05-11 15:03:23 +0700

    remove non-object save method

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ document.addEventListener('DOMContentLoaded', function () {
     var taskName = taskList.querySelector('.js-task-name');
     var task = document.querySelector('.js-task');
     var taskCount = 0;
-    var taskMaxCount = 0;
     var taskInfo = {};
     
 
@@ -18,20 +17,15 @@ document.addEventListener('DOMContentLoaded', function () {
         newTask.classList.remove('task--hidden');
         if (taskName.value !== '') {
             taskCount++;
-            taskMaxCount++;
-            // localStorage.setItem('task_count', taskCount);
-            // localStorage.setItem('task_max_count', taskMaxCount);
-            // localStorage.setItem('task_id_' + taskCount, taskName.value);
             newTask.children[1].value = taskName.value;
             newTask.dataset.taskID = taskCount;
 
-            
             taskInfo['ID_' + newTask.dataset.taskID] = {
+                id: newTask.dataset.taskID,
                 title: newTask.children[1].value,
                 checked: newTask.children[0].children[0].checked
             };
             toLocalStorage('task_info', taskInfo);
-
 
             taskName.value = '';
             taskList.appendChild(newTask);
@@ -47,13 +41,6 @@ document.addEventListener('DOMContentLoaded', function () {
             /* CLOSING TASK*/
             if (evt.target.classList.contains('js-close-button')) {
                 removeTask(this, taskList);
-                taskCount--;
-                if (taskCount <= 0) {
-                    taskMaxCount = 0;
-                    localStorage.setItem('task_max_count', taskMaxCount);
-                }
-                // localStorage.setItem('task_count', taskCount);
-                // localStorage.removeItem('task_id_' + this.dataset.taskID);
                 delete taskInfo['ID_' + this.dataset.taskID];
                 toLocalStorage('task_info', taskInfo);
 
@@ -63,6 +50,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 completeTask(this, 'task--completed', evt.target);
 
                 taskInfo['ID_' + this.dataset.taskID] = {
+                    id: this.dataset.taskID,
                     title: this.children[1].value,
                     checked: this.children[0].children[0].checked
                 };
@@ -77,18 +65,43 @@ document.addEventListener('DOMContentLoaded', function () {
     function loadTasks() {
         var localTaskInfo = localStorage.getItem('task_info');
         localTaskInfo = JSON.parse(localTaskInfo);
+
         for (var key in localTaskInfo) {
             if (localTaskInfo.hasOwnProperty(key)) {
                 var newTask = task.cloneNode(true);
                 newTask.classList.remove('task--hidden');
+                newTask.dataset.taskID = localTaskInfo[key].id;
                 newTask.children[1].value = localTaskInfo[key].title;
                 if (localTaskInfo[key].checked) {
                     newTask.classList.add('task--completed');
                     newTask.children[0].children[0].checked = true;
                 }
                 taskList.appendChild(newTask);
+                
+                newTask.addEventListener('click', function (evt) {
+                    /* CLOSING TASK*/
+                    if (evt.target.classList.contains('js-close-button')) {
+                        removeTask(this, taskList);
+                        delete localTaskInfo['ID_' + this.dataset.taskID];
+                        toLocalStorage('task_info', localTaskInfo);
+        
+                    }
+                    /* COMPLETION TASK*/
+                    if (evt.target.classList.contains('js-complete-button')) {
+                        completeTask(this, 'task--completed', evt.target);
+        
+                        localTaskInfo['ID_' + this.dataset.taskID] = {
+                            id: this.dataset.taskID,
+                            title: this.children[1].value,
+                            checked: this.children[0].children[0].checked
+                        };
+                        toLocalStorage('task_info', localTaskInfo);
+                    }
+                });
             }
         }
+
+
     }
 
 
