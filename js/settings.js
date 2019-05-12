d9ed82a7a9759a4ab4af9a070e57d2c15f724455
kadonomaro <denis.afer@gmail.com>
2019-05-12 20:56:36 +0700

    settings true hiding, added additional styles

--- js/settings.js
+++ js/settings.js
@@ -1,13 +1,24 @@
 var settings = {
     settingsToggle: document.querySelector('.js-settings-toggle'),
     settingsBlock: document.querySelector('.js-settings'),
+    settingsOption: document.querySelectorAll('.js-settings-option'),
     init: function () {
         var _this = this;
+        this.settingsBlock.style.visibility = 'hidden';
         this.settingsToggle.addEventListener('click', function (evt) {
             evt.preventDefault();
             this.classList.toggle('settings-toggle--active');
             _this.settingsBlock.classList.toggle('todo__settings--active');
+            if (_this.settingsBlock.style.visibility === 'hidden') {
+                _this.settingsBlock.style.visibility = 'visible';
+            } else {
+                setTimeout(function () {
+                    _this.settingsBlock.style.visibility = 'hidden';
+                }, 300);
+            }
         });
+
+
     }
 };
 
